b479f266911279a40e358d70a6030da8a6fbb38e
Andrew Joe <andrewajoe@gmail.com>
2013-02-25 23:17:32 -0500

    generators

--- lib/ajo_contact/engine.rb
+++ lib/ajo_contact/engine.rb
@@ -3,6 +3,7 @@ module AjoContact
   class Engine < ::Rails::Engine
     isolate_namespace AjoContact
     paths["config/locales"]
+    paths["lib"]
     generators do
       require "app/lib/generators/ajo_contact/install_generator.rb"
     end
