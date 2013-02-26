1f0ea9d196db904174fab7d72f38432b4eb3adef
Andrew Joe <andrewajoe@gmail.com>
2013-02-25 21:08:33 -0500

    Add generator

---
+++ lib/generators/ajo_contact/view_generator.rb
@@ -0,0 +1,13 @@
+module AjoContact
+  module Generators
+    class ViewGenerator << Rails::Generators::Base
+      desc "Copy over view files"
+
+      source_root File.expand_path('../templates', __FILE__)
+
+      def copy_directories
+        directory 'app/views', 'ajo_contact'
+      end
+    end
+  end
+end
