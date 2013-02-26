e88a118c059d8719f7b8b72b68ed66f673d26303
Andrew Joe <andrewajoe@gmail.com>
2013-02-26 00:29:29 -0500

    generators

--- lib/ajo_contact/engine.rb
+++ lib/ajo_contact/engine.rb
@@ -2,7 +2,7 @@ require 'rails'
 module AjoContact
   class Engine < ::Rails::Engine
     isolate_namespace AjoContact
+    config.autoload_paths << File.expand_path("../lib/generators", __FILE__)
     paths["config/locales"]
-    paths["lib"]
   end
 end
--- lib/generators/ajo_contact/install_generator.rb
+++ lib/generators/ajo_contact/install_generator.rb
@@ -1,16 +1,14 @@
 require 'rails/generators/base'
 module AjoContact
-  module Generators
-    class InstallGenerator << Rails::Generators::Base
-      desc "Copy over view files"
+  class InstallGenerator << Rails::Generators::Base
+    desc "Copy over view files"
 
-      def self.source_root
-        @source_root ||= File.join(File.dirname(__FILE__), 'templates')
-      end
+    def self.source_root
+      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    end
 
-      def copy_directories
-        directory 'app/views', 'ajo_contact'
-      end
+    def copy_directories
+      directory 'app/views', 'ajo_contact'
     end
   end
 end
