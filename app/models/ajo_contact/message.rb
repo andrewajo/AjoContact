b9fbcfc330350943a42beba3c66c45b31bf8e3a4
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-08 02:21:59 -0500

    validate email

--- app/models/ajo_contact/message.rb
+++ app/models/ajo_contact/message.rb
@@ -4,6 +4,7 @@ module AjoContact
     validates_presence_of :first_name, :message => I18n.t('contact.form.error.first_name.required')
     validates_presence_of :last_name, :message => I18n.t('contact.form.error.last_name.required')
     validates_presence_of :email_address, :message => I18n.t('contact.form.error.email_address.required')
+    validates_format_of :email_address, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => I18n.t('contact.form.error.email_address.format')
     validates_presence_of :subject, :message => I18n.t('contact.form.error.subject.required')
     validates_presence_of :message, :message => I18n.t('contact.form.error.message.required')
 
