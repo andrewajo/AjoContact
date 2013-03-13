87af9e04636402f52ac698e1faa1d353a12a0199
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-13 02:16:27 -0400

    validate email

--- app/models/ajo_contact/message.rb
+++ app/models/ajo_contact/message.rb
@@ -1,6 +1,6 @@
 module AjoContact
   class Message < ActiveRecord::Base
-    attr_accessible :first_name, :last_name, :email_address, :subject, :message
+    attr_accessible :first_name, :last_name, :email_address, :subject, :message, :user_agent
     validates_presence_of :first_name, :message => I18n.t('contact.form.error.first_name.required')
     validates_presence_of :last_name, :message => I18n.t('contact.form.error.last_name.required')
     validates_presence_of :email_address, :message => I18n.t('contact.form.error.email_address.required')
