e14755407aa93aa28d3d8f42eb731a121f52ecb1
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-18 18:19:47 -0400

    remove fr

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 AjoContact::Engine.routes.draw do
   root :to => 'contact#index'
-  scope ":locale" do
-    resources :messages, :contact
-    match 'thank_you' => 'contact#thank_you', :as => 'contact_thank_you'
-  end
+  resources :messages, :contact
+  match 'thank_you' => 'contact#thank_you', :as => 'contact_thank_you'
 end
