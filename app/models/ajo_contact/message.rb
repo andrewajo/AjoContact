b1ae291e7eddb9314651987646ae34f1355eeec8
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-19 14:35:12 -0400

    update model

--- app/models/ajo_contact/message.rb
+++ app/models/ajo_contact/message.rb
@@ -1,12 +1,12 @@
 module AjoContact
   class Message < ActiveRecord::Base
     attr_accessible :first_name, :last_name, :email_address, :subject, :message, :user_agent
-    validates_presence_of :first_name, :message => I18n.t('contact.form.error.first_name.required')
-    validates_presence_of :last_name, :message => I18n.t('contact.form.error.last_name.required')
-    validates_presence_of :email_address, :message => I18n.t('contact.form.error.email_address.required')
-    validates_format_of :email_address, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => I18n.t('contact.form.error.email_address.format')
-    validates_presence_of :subject, :message => I18n.t('contact.form.error.subject.required')
-    validates_presence_of :message, :message => I18n.t('contact.form.error.message.required')
+    validates_presence_of :first_name
+    validates_presence_of :last_name
+    validates_presence_of :email_address
+    validates_format_of :email_address, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+    validates_presence_of :subject
+    validates_presence_of :message
 
   end
 end
