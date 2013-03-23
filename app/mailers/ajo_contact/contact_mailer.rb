87f6749edfa084dc3673e64d7254690c6c467a9d
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-23 03:58:24 -0400

    update https and user agent

--- app/mailers/ajo_contact/contact_mailer.rb
+++ app/mailers/ajo_contact/contact_mailer.rb
@@ -2,9 +2,9 @@ module AjoContact
   class ContactMailer < ActionMailer::Base
     default from: "admin@purmoments.heroku.com"
 
-    def contact_email(from, subject, message)
+    def contact_email(from, subject, message, to)
       @message = message
-      mail :to => 'andrew@ajoconsulting.com', :subject => "PURMOMENTS #{subject}", :from => from
+      mail :to => 'andrew@ajoconsulting.com', :subject => "PURMOMENTS #{subject}", :from => from, :to => to
     end
   end
 end
