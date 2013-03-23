e759fd26edf8afee2da978f9e0b51db9fbfca102
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-23 03:35:26 -0400

    update https and user agent

--- app/models/ajo_contact/message.rb
+++ app/models/ajo_contact/message.rb
@@ -1,6 +1,6 @@
 module AjoContact
   class Message < ActiveRecord::Base
-    attr_accessible :first_name, :last_name, :email_address, :subject, :message, :user_agent
+    attr_accessible :first_name, :last_name, :email_address, :subject, :message, :user_agent, :source
     validates_presence_of :first_name
     validates_presence_of :last_name
     validates_presence_of :email_address
